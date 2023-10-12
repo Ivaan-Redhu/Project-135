b34ec63d6611e97062598afea0b15ee595d801a2
Ivaan-Redhu <93902463+Ivaan-Redhu@users.noreply.github.com>
2023-10-12 17:09:22 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status = "";
+objects = [];
 input_text = "";
 
 function setup() {
@@ -21,6 +22,40 @@ function modelLoaded() {
     status = true;
 }
 
-function draw() {
+function gotResult(error, results) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+    objects = results;
+  }
+
+  function draw() {
     image(video, 0, 0, 480, 380);
-}
+        if(status != "")
+        {   
+          objectDetector.detect(video, gotResult);
+          for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+   
+            fill("#20345");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(objects[i].label == input_text){
+              video.stop();
+              object_detector.detect(gotResults);
+              document.getElementById("number_of_objects").innerHTML = input_text+" Found";
+              var synth = window.speechSynthesis;
+              var utterThis = new SpeechSynthesisUtterance(input_text+"found");
+              synth.speak(utterThis);
+            }
+            else{
+              document.getElementById("number_of_objects").innerHTML = input_text+" Not Found";
+            }
+          }
+        }
+  }
